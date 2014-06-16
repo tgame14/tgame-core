9e8ae0a51fcbfedaed6f9fb82e6ae86574642e73
tgame14 <tgameBusiness@gmail.com>
2014-06-16 23:29:21 +0300

    Finished integration across

--- src/main/java/com/tgame/mods/libs/multiblocks/simpleimpl/chest/TileInventoryMultiblock.java
+++ src/main/java/com/tgame/mods/libs/multiblocks/simpleimpl/chest/TileInventoryMultiblock.java
@@ -4,6 +4,8 @@ import com.tgame.mods.libs.inventory.IInventoryHandler;
 import com.tgame.mods.libs.multiblocks.MultiblockValidationException;
 import com.tgame.mods.libs.multiblocks.grid.GridController;
 import com.tgame.mods.libs.multiblocks.simpleimpl.TileSimpleNode;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -13,7 +15,7 @@ import java.util.List;
  * @author tgame14
  * @since 16/06/14
  */
-public class TileInventoryMultiblock extends TileSimpleNode implements IInventoryHandler
+public class TileInventoryMultiblock extends TileSimpleNode implements IInventory, IInventoryHandler
 {
 
 	@Override
@@ -124,4 +126,85 @@ public class TileInventoryMultiblock extends TileSimpleNode implements IInventor
 
 	}
 
+	@Override
+	public int getSizeInventory()
+	{
+		return this.getInventoryHandler().getSizeInventory(ForgeDirection.UNKNOWN);
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int index)
+	{
+		// a bandage on a bigger problem.. placeholder until 1.8
+		List<ItemStack> list = this.getInventoryHandler().getInventoryContents(ForgeDirection.UNKNOWN);
+		return list.get(index);
+	}
+
+	@Override
+	public ItemStack decrStackSize(int var1, int var2)
+	{
+		List<ItemStack> list = this.getInventoryHandler().getInventoryContents(ForgeDirection.UNKNOWN);
+		ItemStack stack = list.get(var1).copy();
+		stack.stackSize = var2;
+		return this.getInventoryHandler().extractItem(ForgeDirection.UNKNOWN, stack, false);
+
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int var1)
+	{
+		return getStackInSlot(var1);
+	}
+
+	@Override
+	public void setInventorySlotContents(int var1, ItemStack var2)
+	{
+		List<ItemStack> list = this.getInventoryHandler().getInventoryContents(ForgeDirection.UNKNOWN);
+		this.getInventoryHandler().extractItem(ForgeDirection.UNKNOWN, list.get(var1), false);
+		this.getInventoryHandler().insertItem(ForgeDirection.UNKNOWN, var2, false);
+
+
+	}
+
+	@Override
+	public String getInventoryName()
+	{
+		return this.getClass().getSimpleName();
+	}
+
+	@Override
+	public boolean hasCustomInventoryName()
+	{
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit()
+	{
+		return Integer.MAX_VALUE;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer var1)
+	{
+		return true;
+	}
+
+	@Override
+	public void openInventory()
+	{
+
+	}
+
+	@Override
+	public void closeInventory()
+	{
+
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int var1, ItemStack var2)
+	{
+		return this.getInventoryHandler().isFull(ForgeDirection.UNKNOWN);
+	}
 }
