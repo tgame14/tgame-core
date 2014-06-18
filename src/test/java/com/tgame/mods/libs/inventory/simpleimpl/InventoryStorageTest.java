7658ae6d2d96d2ff7657203408990a4afd0d6e36
tgame14 <tgameBusiness@gmail.com>
2014-06-18 12:09:25 +0300

    Added unit testing lib JUnit to testCompile deps

---
+++ src/test/java/com/tgame/mods/libs/inventory/simpleimpl/InventoryStorageTest.java
@@ -0,0 +1,67 @@
+package com.tgame.mods.libs.inventory.simpleimpl;
+
+import static org.junit.Assert.*;
+
+public class InventoryStorageTest
+{
+
+	@org.junit.Test
+	public void testInsertItem() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testExtractItem() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testExtractItem1() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testGetInventoryContents() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testGetSizeInventory() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testIsEmpty() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testIsFull() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testWriteToNBT() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testReadFromNBT() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testOnInventoryChanged() throws Exception
+	{
+
+	}
+}
