e71907d0872a9d26aaec3a6ac96c91cac7b1a2ee
tgame14 <tgameBusiness@gmail.com>
2014-08-25 00:25:43 +0300

    Updating and refreshing basics

---
+++ src/main/java/com/tgame/mods/libs/render/TGBlockRenderer.java
@@ -0,0 +1,37 @@
+package com.tgame.mods.libs.render;
+
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.world.IBlockAccess;
+
+/**
+ * @author tgame14
+ * @since 13/07/2014
+ */
+public class TGBlockRenderer implements ISimpleBlockRenderingHandler
+{
+	@Override
+	public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer)
+	{
+
+	}
+
+	@Override
+	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean shouldRender3DInInventory(int modelId)
+	{
+		return false;
+	}
+
+	@Override
+	public int getRenderId()
+	{
+		return 0;
+	}
+}
